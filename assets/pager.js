8e5f55fadb73d14ec254c39ba6fa9b2f37a2ea09
Pavel Veselov <pvsaintpe@icloud.com>
2018-10-31 09:22:55 +0300

    fix log js

--- assets/pager.js
+++ assets/pager.js
@@ -5,10 +5,18 @@ $(function() {
         $.pjax({url: url, container: '#' + id});
     }).on('change', 'select.page-size', function () {
         var currentPage = $(this).attr('data-page');
+        console.log('current-page: ', currentPage);
         var currentPerPage = $(this).attr('data-per-page');
+        console.log('current-per-page: ', currentPerPage);
+        console.log('current-page(-1): ', (currentPage - 1));
+        console.log('current-page(-1)*current-per-page: ', (currentPage - 1) * currentPerPage);
+        console.log('new-page: ', $(this).val());
         var calcPage = ((currentPage - 1) * currentPerPage) / $(this).val();
+        console.log('calc-page: ', calcPage);
         var page = Math.ceil(calcPage) + 1;
+        console.log('round-page(+1): ', page);
         var url = $(this).data('url') + '&page=' + page + '&per-page=' + $(this).val();
+        console.log('url: ', url);
         var id = $(this).closest('div[data-pjax-container]').attr('id');
         $.pjax({url: url, container: '#' + id});
     });
