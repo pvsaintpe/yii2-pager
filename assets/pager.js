4d87398de0ba8debea1b6286db279682d386f6d3
Grimailo Artem <grimailo.am@spb.rusoft-company.ru>
2018-12-10 15:30:15 +0300

    add TouchSpin/kartik, change css,js

--- assets/pager.js
+++ assets/pager.js
@@ -1,31 +1,34 @@
-$(function() {
-    $('body').on('change', 'input.page-selector', function () {
-        var url = $(this).data('url') + '&page=' + $(this).val();
-        var container = $(this).closest('div[data-pjax-container]');
-        if (container.length) {
-            $.pjax({url: url, container: '#' + container.attr('id')});
-        } else {
-            location.href = url;
-        }
-    }).on('change', 'select.page-size', function () {
-        var currentPage = $(this).attr('data-page');
-        // console.log('current-page: ', currentPage);
-        var currentPerPage = $(this).attr('data-per-page');
-        // console.log('current-per-page: ', currentPerPage);
-        // console.log('current-page(-1): ', (currentPage - 1));
-        // console.log('current-page(-1)*current-per-page: ', (currentPage - 1) * currentPerPage);
-        // console.log('new-page: ', $(this).val());
-        var calcPage = ((currentPage - 1) * currentPerPage) / $(this).val();
-        // console.log('calc-page: ', calcPage);
-        var page = Math.ceil(calcPage) + 1;
-        // console.log('round-page(+1): ', page);
-        var url = $(this).data('url') + '&page=' + page + '&per-page=' + $(this).val();
-        // console.log('url: ', url);
-        var container = $(this).closest('div[data-pjax-container]');
-        if (container.length) {
-            $.pjax({url: url, container: '#' + container.attr('id')});
-        } else {
-            location.href = url;
+$(function () {
+
+    $('body').on('click', function (e) {
+        var selectContainer = $('.pv_input');
+        if (e.target.classList.contains('bootstrap-touchspin-up') ||
+            e.target.classList.contains('bootstrap-touchspin-down')) {
+            selectContainer.focus();
         }
     });
+
+    $('body').keydown(function (e) {
+        if (e.keyCode == 13 && e.target.classList.contains('pv_input')) {
+            var selectContainer = $('.pv_input');
+            var maxPageSize = parseInt(selectContainer.attr('data-max-page'), 10);
+            var minPageSize = parseInt(selectContainer.attr('data-min-page'), 10);
+            var pageNumber = parseInt(selectContainer.val(), 10);
+            if (pageNumber > maxPageSize) {
+                pageNumber = maxPageSize
+            } else if (pageNumber < minPageSize) {
+                pageNumber = minPageSize
+            }
+            var url = selectContainer.attr('data-url');
+            url += pageNumber;
+
+            var container = selectContainer.closest('div[data-pjax-container]');
+            if (container.length) {
+                $.pjax({url: url, container: '#' + container.attr('id')});
+            } else {
+                location.href = url;
+            }
+        }
+    });
+
 });
